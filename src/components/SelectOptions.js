f05b7d4fe676238cc4af69274e2ea73d32756f03
Yogita Verma <veryogita@gmail.com>
2022-01-23 21:07:59 +0530

    Update SelectOptions.js

--- src/components/SelectOptions.js
+++ src/components/SelectOptions.js
@@ -1,11 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+/**
+ * Component that renders a select box
+ * @returns JSX Element
+ */
 const SelectOptions = ({ options, selectedOption, setSelectedOption }) => {
   return (
     <>
       <select
-        className="select_box"
+        className="select_box mb-1 mr-2 pb-1 pt-1 cursor-pointer"
         value={selectedOption}
         onChange={(e) => setSelectedOption(e.target.value)}
       >
