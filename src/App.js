2ca5ebaa344ffe005530cc3ff78062c9530e31a5
Yogita Verma <veryogita@gmail.com>
2022-01-23 21:03:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import Modal from "./components/Modal";
 import { data } from "./peopleData";
 
+/**
+ * Main Component that renders Modal
+ * @returns JSX Element
+ */
 const App = () => {
   const [peopleData, setPeopleData] = React.useState(data);
   return (
@@ -12,3 +16,4 @@ const App = () => {
 };
 
 export default App;
+
