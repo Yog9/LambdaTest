c5bc9c33b496ff6f4d6547fee23f26af8aa083bd
Yogita Verma <veryogita@gmail.com>
2022-01-23 21:05:18 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
+/**
+ * Component that renders a Header
+ * @returns JSX Element
+ */
 const Header = () => {
     return (
         <div>
-            <div className="grid grid-cols-3 pt-4 pb-2 text-zinc-500">
-                <div>EMAIL</div>
+            <div className="grid grid-cols-4 pt-4 pb-2 text-zinc-500 justify-center gap-1">
+                <div className='col-span-2 pl-2'>EMAIL</div>
                 <div>ROLE</div>
                 <div>GROUP</div>
             </div>
