6402a140f1a905d3e0b230c81c7704af588f874e
Yogita Verma <veryogita@gmail.com>
2022-01-23 21:08:26 +0530

    Update Switch.js

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+/**
+ * Switch Component that renders a toggle
+ * @returns JSX Element
+ */
 const Switch = ({ toggle, setToggle }) => {
   return (
     <>
@@ -9,8 +13,8 @@ const Switch = ({ toggle, setToggle }) => {
       </div>
       <div
         className={
-          "md:w-14 md:h-7 w-12 h-6 flex items-center bg-emerald-400 rounded-full cursor-pointer" +
-          (toggle ? " bg-emerald-400" : " bg-slate-300")
+          "w-14 md:h-6 w-10 h-6 flex items-center bg-emerald-400 rounded-full cursor-pointer" +
+          (toggle ? " bg-teal-400" : " bg-slate-300")
         }
         onClick={() => {
           setToggle(!toggle);
@@ -18,8 +22,8 @@ const Switch = ({ toggle, setToggle }) => {
       >
         <div
           className={
-            "bg-white md:w-6 md:h-6 h-5 w-5 rounded-full shadow-md transform duration-300 ease-in-out" +
-            (toggle ? " transform translate-x-6" : " transform translate-2")
+            "bg-white md:w-5 md:h-5 h-5 w-5 rounded-full shadow-md transform duration-300 ease-in-out" +
+            (toggle ? " transform translate-x-5" : " transform translate-2")
           }
         ></div>
       </div>
